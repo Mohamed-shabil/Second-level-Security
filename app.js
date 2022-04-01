155cc1916a7566a710fba9b509b3e5d7d50c55b3
Mohamed Shabil <you@example.com>
2022-04-01 20:57:21 +0530

    Secrets

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ mongoose.connect('mongodb://localhost:27017/userDB',{useNewUrlParser: true});
 
 const userSchema= new mongoose.Schema({
     email:String,
-    password:String
+    password:String,
+    secret : String
 })
 userSchema.plugin(passportLocalMongoose);
  
@@ -54,17 +55,28 @@ app.get('/register', (req, res) => {
 })
   
 app.get('/secrets',(req, res)=>{
-     if (req.isAuthenticated()) {
-         res.render('secrets');
-     }else{
-         res.redirect('/login');
-     }
+    User.find({secret:{$ne:null}},(err,foundUsers)=>{
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUsers){
+                res.render('secrets',{usersWithSecret : foundUsers})
+            }
+        }
+    })
 })
 app.get('/logout', (req, res)=>{
     req.logout();
     res.redirect('/')
 })
+app.get('/submit', (req, res)=>{
+    if (req.isAuthenticated()) {
+        res.render('submit');
 
+    }else{
+        res.redirect('/login');
+    }
+})
 app.post('/register', (req, res) => {
 
     User.register({username: req.body.username}, req.body.password,(err,user)=>{
@@ -93,6 +105,23 @@ app.post('/login',(req, res)=>{
            })
        }
    })
+})
+
+app.post('/submit',(req, res)=>{
+    const submittedSecret = req.body.secret;
+    console.log(submittedSecret);
+    console.log(req.user.id);
+
+    User.findById(req.user.id,(err,foundUser)=>{
+        if (err) {
+            console.log(err)
+        }else{
+            foundUser.secret =submittedSecret;
+            foundUser.save(()=>{
+                res.redirect('/secrets');
+            })
+        }
+    })
 })
 
 
